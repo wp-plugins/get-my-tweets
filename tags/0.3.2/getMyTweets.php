7e2d1075933684b889b5a5060b0e0642b36882d0
south_platte <south_platte@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-02-14 00:36:14 +0000

    tagging version 0.3.2
    git-svn-id: https://plugins.svn.wordpress.org/get-my-tweets@94463 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/0.3.2/getMyTweets.php
@@ -0,0 +1,125 @@
+<?php
+/*
+Plugin Name: getMyTweets
+Plugin URI: http://www.southplattewebdesign.com/getMyTweets
+Description: Simple plugin to return a user defined number of tweets from Twitter
+Version: 0.3.2
+Author: Billy Nab
+Author URI: http://www.southplattewebdesign.com
+Copyright 2008  Billy Nab  (email : bnab@southplattewebdesign.com)
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+Version Changes:
+
+0.1.1 - Initial version, no URL parsing, no widget
+0.2.1 - Widgetized version
+0.3.1 - Added ability to parse one url in tweets
+0.3.2 - Modified URL parsing method
+*/
+function get_my_tweets_menu() {
+  add_options_page( 'Get My Tweets', 'Get My Tweets', 8, __FILE__, 'get_my_tweets_options' );
+}
+
+function get_my_tweets() {
+	$twit_usr = get_option( 'twitter_user_name' );
+	$num_tweets = get_option( 'num_tweets_retrieve' );
+	$reader = new XMLReader();
+	$reader->open( 'http://twitter.com/statuses/user_timeline/'.$twit_usr.'.xml?count='.$num_tweets );
+	while ( $reader->read() ) 
+	{
+	   if ( $reader->nodeType == XMLREADER::ELEMENT ) 
+	   {
+	   	  $name = $reader->name;
+	      if( $name == "status" )
+		  {
+		  	while( $reader->read() )
+			{
+				if( $reader->nodeType == XMLREADER::ELEMENT )
+				{
+					$noName = $reader->name;
+					if( $noName == "text" )
+					{
+						$reader->read();
+						$checkUrl = $reader->value;
+						$pattern = "/ http:\/\/(.*?)\/(.*? )/";
+						$replacement = "<a href=\"$0\" target=\"_blank\">$0</a>";
+						$finalString = preg_replace( $pattern, $replacement, $checkUrl );
+						//$finalString = ereg_replace("[[:space:]]+[[:alpha:]]+://[^<>[:space:]]+[[:alnum:]/]+[[:alnum:]]+[[:space:]]", "<a href=\"\\0\">\\0</a>", $checkUrl);
+						$result .= $finalString . '<br><br>';
+						//break;
+					}//close if noName match
+					if( $noName == "created_at" )
+					{
+						$reader->read();
+						$tDate = date_parse($reader->value);
+						if($tDate['minute'] < 10)
+						{
+							$tDate['minute'] = '0'.$tDate['minute'];
+						}
+						$tweetdate = $tDate['month'].'/'.$tDate['day'].'/'.$tDate['year'].' '.$tDate['hour'].':'.$tDate['minute'];
+						$result .= $tweetdate . '<br>';
+					}
+				}//close if nodeType
+			}//close while reader read inner
+		  }//close if name item  
+	   }//close if nodeType
+	}//close while reader-read outer
+	return $result;
+	$reader->close();//close xmlreader
+}//end function
+// internal testing $myTweets = getMyTweets();
+
+function get_my_tweets_options() {
+	?>
+	<div class="wrap">
+	<h2>Get My Tweets</h2>	
+	<form method="post" action="options.php">	
+	<table class="form-table">
+	<tr valign="top">
+	<th scope="row">Twitter User Name</th>
+	<td><input type="text" name="twitter_user_name" value="<?php echo get_option( 'twitter_user_name' ); ?>" /></td>
+	</tr>	 
+	<tr valign="top">
+	<th scope="row">Number Of Tweets To Retrieve</th>
+	<td><input type="text" name="num_tweets_retrieve" value="<?php echo get_option( 'num_tweets_retrieve' ); ?>" /></td>
+	</tr>	
+	</table>	
+	<input type="hidden" name="action" value="update" />
+	<input type="hidden" name="page_options" value="twitter_user_name,num_tweets_retrieve" />
+	<?php wp_nonce_field( 'update-options' ); ?>	
+	<p class="submit">
+	<input type="submit" name="Submit" value="<?php _e( 'Save Changes' ) ?>" />
+	</p>
+	</form>
+	</div>
+	<?php 
+	}//end get_my_tweets_options
+	
+	function get_my_tweets_widget_init() {
+
+		function widget_get_my_tweets( $args )
+		{
+		    extract( $args );
+		    echo $before_widget;
+		    echo $before_title . 'Recent Tweets' . $after_title;
+		    echo get_my_tweets();
+		    echo $after_widget;
+		}
+		if ( !function_exists('register_sidebar_widget') ) return;
+			register_sidebar_widget( array( 'Get My Tweets', 'widgets' ), 'widget_get_my_tweets' );
+	}
+add_action ( 'admin_menu', 'get_my_tweets_menu');	
+add_action( 'plugins_loaded',  'get_my_tweets_widget_init' );
+?>
---
+++ tags/0.3.2/readme.txt
@@ -0,0 +1,30 @@
+=== Get My Tweets ===
+Contributors: south_platte
+Donate link: http://www.southplattewebdesign.com/getmytweets-wordpress-plugin/
+Tags: twitter, xmlreader, tweets, twitter plugin
+Requires at least: 2.7
+Tested up to: 2.7
+Stable tag: 0.3.2
+
+Simple plugin to return a user defined number of tweets from Twitter and parse any URLs in the tweet.
+
+== Description ==
+
+Using PHP 5 and XMLReader, this plugin will load a user definable number of Tweets from Twitter.  It requires that the Twitter user name be supplied to the plugin, and the desired number of Tweets to retrieve.  It also requires that user's Twitter timeline be "public" to be able to be parsed and read.  The plugin will parse links within Tweets.
+
+== Installation ==
+
+1. Upload `getMyTweets.php` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. You can add the widget through the 'Widgets' menu under 'Appearance'
+1. If you are not using themes, you can add `<?php get_my_tweets();?>` to your theme's files where you want your tweets to show up.
+
+== Frequently Asked Questions ==
+
+= Is there a specific PHP version? =
+
+Yes, Get My Tweets requires PHP version 5 or higher with the XMLReader extension loaded
+
+== Screenshots ==
+
+1. Get My Tweets options.
